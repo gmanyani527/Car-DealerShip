d199548773cd9576f9a0e84584a6e4ce1ab1bca6
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-09 16:27:26 -0400

    Wrote the basic method frameworks for Dealership and Vehicle

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+public class Dealership {
+    private String name;
+    private String addresss;
+    private String phone;
+
+
+    public Dealership(String name, String addresss, String phone) {
+        this.name = name;
+        this.addresss = addresss;
+        this.phone = phone;
+    }
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByMakeModel(String make, String model){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByYear(double min, double max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByColor(String color){
+
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByMileage(double min, double max){
+
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByType(String vehicleType){
+
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles(){
+
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+
+    }
+
+
+
+}
