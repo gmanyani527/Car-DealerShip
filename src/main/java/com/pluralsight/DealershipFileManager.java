b74d5a90e46c3ff0bb804d315676b6a7d4eee355
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-10 22:44:40 -0400

    Worked on the getDealership method

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,12 +1,59 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class DealershipFileManager {
 
-public Dealership getDealership(){
-    return null;
-}
+    public Dealership getDealership() {
+        Dealership dealership = null;
 
-public void saveDealership(Dealership){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("dealership.csv"));
+            String line;
 
-}
+            if ((line = br.readLine()) != null) {
+                String[] dealerInfo = line.split(",");
+                String name = dealerInfo[0];
+                String address = dealerInfo[1];
+                String phone = dealerInfo[2];
+
+                dealership = new Dealership(name, address, phone);
+            }
+
+            while ((line = br.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    continue; // skip empty lines
+                }
+                String[] parts = line.split("\\|");
+                if (parts.length != 5) {
+                    System.out.println("Skipping bad line: " + line);
+                    continue; // skip invalid lines
+                }
+                int vin = Integer.parseInt(parts[0]);
+                String make = parts[1];
+                int year = Integer.parseInt(parts[2]);
+                String model = parts[3];
+                String vehicleType = parts[4];
+                String color = parts[5];
+                int odometer = Integer.parseInt(parts[6]);
+                double price = Double.parseDouble(parts[7]);
+
+
+                Vehicle vehicle = new Vehicle(vin, make, year, model, vehicleType, color, odometer, price);
+                dealership.addVehicle(vehicle);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return dealership;
+    }
+
+    public static ArrayList<Dealership> saveDealership() {
+        return null;
+    }
 }
