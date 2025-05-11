8971453bc5b22bed133a79b8cc85686ee78bd64e
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-11 02:42:05 -0400

    added the interFace into the Program class and should display when it runs

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Program {
+
+    public static void main(String[] args) {
+        UserInterface interFace = new UserInterface();
+
+        interFace.display();
+    }
+
+
+
+
 }
