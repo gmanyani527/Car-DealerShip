3a794a12ce1f9972c28305c8e983f9e736a513ff
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-05-11 13:31:58 -0400

    Updated all the getter method from the Dealership just have to do the addVehicle and removevehicle

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -6,45 +6,82 @@ public class Dealership {
     private String name;
     private String addresss;
     private String phone;
-
+    ArrayList<Vehicle> inventory;
 
     public Dealership(String name, String addresss, String phone) {
         this.name = name;
         this.addresss = addresss;
         this.phone = phone;
-        ArrayList<String> arrayList = new ArrayList<>();
+        this.inventory = new ArrayList<>();
 
     }
     public ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
-        return null;
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            if (vehicle.getPrice() >= min && vehicle.getPrice() <= max){
+                results.add(vehicle);
+            }
+        }
+
+        return results;
     }
 
     public ArrayList<Vehicle> getVehicleByMakeModel(String make, String model){
-        return null;
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            if (vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)){
+                results.add(vehicle);
+            }
+        }
+        return results;
     }
 
-    public ArrayList<Vehicle> getVehicleByYear(double min, double max){
-        return null;
+    public ArrayList<Vehicle> getVehicleByYear(int min, int max){
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            if (vehicle.getYear() > min && vehicle.getYear() < max){
+                results.add(vehicle);
+            }
+        }
+        return results;
     }
 
     public ArrayList<Vehicle> getVehicleByColor(String color){
-
-        return null;
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            if (vehicle.getColor().equalsIgnoreCase(color)){
+                results.add(vehicle);
+            }
+        }
+        return results;
     }
 
     public ArrayList<Vehicle> getVehicleByMileage(double min, double max){
-
-        return null;
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            if (vehicle.getOdometer() > min && vehicle.getOdometer() < max){
+                results.add(vehicle);
+            }
+        }
+        return results;
     }
 
     public ArrayList<Vehicle> getVehicleByType(String vehicleType){
-
-        return null;
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            if (vehicle.getVehicleType().equalsIgnoreCase(vehicleType)){
+                results.add(vehicle);
+            }
+        }
+        return results;
     }
 
     public ArrayList<Vehicle> getAllVehicles(){
-
-        return null;
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle: inventory){
+            results.add(vehicle);
+        }
+        return results;
     }
 
     public void addVehicle(Vehicle vehicle){
